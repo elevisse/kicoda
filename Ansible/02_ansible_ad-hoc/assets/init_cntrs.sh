af01daa34b476044317895ed2030c0b3ff642314
etienne_levisse <etienne.levisse@gmail.com>
2023-10-06 14:53:08 +0200

    Add: sshpass

--- Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
+++ Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 apt update -y
-apt upgrade -y
 apt install sshpass -y
 docker-compose -f /root/docker-compose.yml up -d
 echo "10.1.1.1 node01" >> /etc/hosts
 echo "10.1.1.2 node02" >> /etc/hosts
 echo "10.1.1.3 node03" >> /etc/hosts
 pip install ansible
-echo "10.1.1.1 node01" > /etc/ansible/hosts
-echo "10.1.1.2 node02" >> /etc/ansible/hosts
-echo "10.1.1.3 node03" >> /etc/ansible/hosts
+echo "node01" > /etc/ansible/hosts
+echo "node02" >> /etc/ansible/hosts
+echo "node03" >> /etc/ansible/hosts
+sshpass -p ubuntu ssh-copy-id root@node01
+sshpass -p ubuntu ssh-copy-id root@node02
+sshpass -p ubuntu ssh-copy-id root@node03
