42ff2ac709641e2f907982930206022f10a9a94e
etienne_levisse <etienne.levisse@gmail.com>
2023-10-06 14:44:57 +0200

    MOD init ansible

--- Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
+++ Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
@@ -7,4 +7,8 @@ docker-compose -f /root/docker-compose.yml up -d
 echo "10.1.1.1 node01" >> /etc/hosts
 echo "10.1.1.2 node02" >> /etc/hosts
 echo "10.1.1.3 node03" >> /etc/hosts
+pip install ansible
+echo "10.1.1.1 node01" > /etc/ansible/hosts
+echo "10.1.1.2 node02" >> /etc/ansible/hosts
+echo "10.1.1.3 node03" >> /etc/ansible/hosts
 touch /root/.finished
