eafa94c2dff868b2a63cb5c5f8b1087d8f673b16
etienne_levisse <etienne.levisse@gmail.com>
2023-10-06 15:15:31 +0200

    DBG: inventory

--- Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
+++ Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
@@ -7,6 +7,7 @@ echo "10.1.1.1 node01" >> /etc/hosts
 echo "10.1.1.2 node02" >> /etc/hosts
 echo "10.1.1.3 node03" >> /etc/hosts
 pip install ansible
+mkdir -p /etc/ansible
 echo "[myctnzdmachines]" > /etc/ansible/hosts
 echo "node01" >> /etc/ansible/hosts
 echo "node02" >> /etc/ansible/hosts
