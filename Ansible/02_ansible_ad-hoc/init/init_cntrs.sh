9f47ac0c9cbdd723b54a2c6b3aca8fcd10068d1c
etienne_levisse <etienne.levisse@gmail.com>
2023-10-06 14:18:40 +0200

    DBG: init

--- Ansible/02_ansible_ad-hoc/init/init_cntrs.sh
+++ Ansible/02_ansible_ad-hoc/init/init_cntrs.sh
@@ -3,11 +3,11 @@
 apt update -y
 apt upgrade -y
 apt install sshpass -y
-docker-compose up -f /root/docker-compose.yml -d
-sshpass -p ubuntu ssh-copy-id root@10.1.1.1
-sshpass -p ubuntu ssh-copy-id root@10.1.1.2
-sshpass -p ubuntu ssh-copy-id root@10.1.1.3
+docker-compose -f /root/docker-compose.yml up -d
 echo "10.1.1.1 node01" >> /etc/hosts
 echo "10.1.1.2 node02" >> /etc/hosts
 echo "10.1.1.3 node03" >> /etc/hosts
+sshpass -p ubuntu ssh-copy-id root@node01
+sshpass -p ubuntu ssh-copy-id root@node02
+sshpass -p ubuntu ssh-copy-id root@node03
 touch /root/.finished
