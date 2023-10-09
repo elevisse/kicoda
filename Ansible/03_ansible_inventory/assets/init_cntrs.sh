2b4178f4c7bf66b9618d7c66886d0eb2080597a5
etienne_levisse <etienne.levisse@gmail.com>
2023-10-09 13:20:50 +0200

    RFC: start script lab

--- Ansible/03_ansible_inventory/assets/init_cntrs.sh
+++ Ansible/03_ansible_inventory/assets/init_cntrs.sh
@@ -3,14 +3,9 @@
 apt update -y
 apt install sshpass -y
 docker-compose -f /root/docker-compose.yml up -d
-echo "10.1.1.1 node01" >> /etc/hosts
-echo "10.1.1.2 node02" >> /etc/hosts
-echo "10.1.1.3 node03" >> /etc/hosts
+for n_node in {1..3}; do
+    echo "10.1.1.$n_node node0$n_node" >> /etc/hosts
+    ssh-keyscan node0$n_node >> ~/.ssh/known_hosts
+    sshpass -p ubuntu ssh-copy-id root@node0$n_node
+done
 pip install ansible
-mkdir -p /etc/ansible
-ssh-keyscan node01 >> ~/.ssh/known_hosts
-ssh-keyscan node02 >> ~/.ssh/known_hosts
-ssh-keyscan node03 >> ~/.ssh/known_hosts
-sshpass -p ubuntu ssh-copy-id root@node01
-sshpass -p ubuntu ssh-copy-id root@node02
-sshpass -p ubuntu ssh-copy-id root@node03
