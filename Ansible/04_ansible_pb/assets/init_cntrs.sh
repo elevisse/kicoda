28fda41d5a1dbcd4eb0c575d91198e0a2e349b42
etienne_levisse <etienne.levisse@gmail.com>
2023-10-09 16:06:51 +0200

    DBG: script

--- Ansible/04_ansible_pb/assets/init_cntrs.sh
+++ Ansible/04_ansible_pb/assets/init_cntrs.sh
@@ -16,7 +16,7 @@ docker-compose -f /root/docker-compose.yml up -d
 for n_node in {1..4}; do
     echo "10.1.1.$n_node node0$n_node" >> /etc/hosts
     ssh-keyscan node0$n_node >> ~/.ssh/known_hosts
-    if [ $n_node=2 ]; then
+    if [ $n_node -eq 2 ]; then
         sshpass -p root ssh-copy-id root@node0$n_node
     else
         sshpass -p ubuntu ssh-copy-id root@node0$n_node
