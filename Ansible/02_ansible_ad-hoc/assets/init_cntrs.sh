ea0b5382c24deee00067039f8fa459a2d269edce
etienne_levisse <etienne.levisse@gmail.com>
2023-10-09 13:19:20 +0200

    MOD: set lab by script

--- Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
+++ Ansible/02_ansible_ad-hoc/assets/init_cntrs.sh
@@ -2,19 +2,13 @@
 
 apt update -y
 apt install sshpass -y
-docker-compose -f /root/docker-compose.yml up -d
-echo "10.1.1.1 node01" >> /etc/hosts
-echo "10.1.1.2 node02" >> /etc/hosts
-echo "10.1.1.3 node03" >> /etc/hosts
-pip install ansible
 mkdir -p /etc/ansible
 echo "[myctnzdmachines]" > /etc/ansible/hosts
-echo "node01" >> /etc/ansible/hosts
-echo "node02" >> /etc/ansible/hosts
-echo "node03" >> /etc/ansible/hosts
-ssh-keyscan node01 >> ~/.ssh/known_hosts
-ssh-keyscan node02 >> ~/.ssh/known_hosts
-ssh-keyscan node03 >> ~/.ssh/known_hosts
-sshpass -p ubuntu ssh-copy-id root@node01
-sshpass -p ubuntu ssh-copy-id root@node02
-sshpass -p ubuntu ssh-copy-id root@node03
+docker-compose -f /root/docker-compose.yml up -d
+for n_node in {1..3}; do
+    echo "10.1.1.$n_node node0$n_node" >> /etc/hosts
+    echo "node0$n_node" >> /etc/ansible/hosts
+    ssh-keyscan node0$n_node >> ~/.ssh/known_hosts
+    sshpass -p ubuntu ssh-copy-id root@node0$n_node
+done
+pip install ansible
