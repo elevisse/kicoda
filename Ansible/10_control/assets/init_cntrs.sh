cbd4ab36da4d49ea9a661dbee7060088e6a5d6c2
etienne_levisse <etienne.levisse@gmail.com>
2024-11-18 18:46:06 +0100

    DBG: lab

--- Ansible/10_control/assets/init_cntrs.sh
+++ Ansible/10_control/assets/init_cntrs.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+apt update -y
+apt install sshpass python3.9 ca-certificates curl -y
+update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt remove $pkg -y; done
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update -y
-apt install sshpass python3.9 -y
-update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y -q -o DPkg::Options::=--force-confdef
+
 unlink /usr/bin/python
 ln -s /usr/bin/python3.9 /usr/bin/python
 unlink /usr/bin/python3
 ln -s /usr/bin/python3.9 /usr/bin/python3
+
 pip install ansible cffi
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.docker
